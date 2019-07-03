36a5ffb37fc435c2bbc3e1431424cc3f635036e4
Lari Taskula <lari@taskula.fi>
2019-07-03 03:23:29 +0300

    Bump version to 0.003

--- lib/Taskula.pm
+++ lib/Taskula.pm
@@ -16,7 +16,7 @@ package Taskula;
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 
 use strict;
 use warnings;
